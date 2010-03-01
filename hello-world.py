0803fb6a8a93d1400ea1d0c77da3e54fa6c5f239
Robert (Jamie) Munro <rjmunro@mythbuntu.arjam.net>
2010-03-01 23:41:23 +0000

    Made minimal changes to make it work with latest version.

--- hello-world.py
+++ hello-world.py
@@ -4,19 +4,23 @@ import time
 
 import freej
 
-# initializes FreeJ creating a Contex
+# initializes FreeJ creating a Context
 cx = freej.Context()
 
-# creates a screen of given size
-scr = freej.SdlScreen( 400, 300 )
+# creates a screen (different to docs)
+scr = freej.SdlScreen()
+
+# Initialise the screen to the given size (I chose Widescreen PAL resolution) (not in docs)
+scr.init(1024,576,32)
+
 # adds the screen
 cx.add_screen(scr)
 
 # create an instance of a TextLayer
 txt = freej.TextLayer()
 
-# initializes the new layer with the freej context
-txt.init(cx)
+# initializes the new layer with the freej context (different to docs - optionally takes x,y,bpp size)
+txt.init()
 
 # writes the hello world text inside the layer
 txt.write("Hello World!")
